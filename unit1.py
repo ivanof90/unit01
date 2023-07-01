7a76ed3170b421b21e7f6b3c8baac7a3c65088f5
ivan_conde <ivanof90@gmail.com>
2023-07-01 04:00:04 +0000

    cleaning file

--- unit1.py
+++ unit1.py
@@ -7,23 +7,9 @@ from stable_baselines3 import PPO
 #env = make_vec_env('LunarLander-v2', n_envs=16)
 
 env = gym.make('LunarLander-v2',render_mode="human")
-#env = gym.make('LunarLander-v2')
 
-# Instantiate the agent
-#model = PPO('MlpPolicy', env, verbose=1, n_steps = 1024,
-#    batch_size = 64,
-#    n_epochs = 4,
-#    gamma = 0.999,
-#    gae_lambda = 0.98,
-#    ent_coef = 0.01)
-#print("training the model")
-#model.learn(total_timesteps=int(1e6))
-#model.save("moon_env")
-#print("training complete")
-#del model # remove to demonstrate saving and loading
 model = PPO.load("moon_env")
 obs = env.reset()[0]
-#env.render()
 while True:
     action, _states = model.predict(obs)
     obs, rewards, terminated, truncated, info = env.step(action)
